347b6251c6d7f179cdcb9865101d537a6338ab68
Calen Pennington <cale@edx.org>
2019-03-25 13:05:00 -0400

    Correctly un-hide vis

--- main.js
+++ main.js
@@ -677,7 +677,7 @@ function comparePlayers(player, opponent) {
     vegaEmbed("#vis", vlC1C2)
       .then(function() {
         loading.style.display = "none";
-        delete vis.style.display;
+        vis.style.display = "";
       })
       .catch(console.error);
   }, 10);
